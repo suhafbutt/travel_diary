932f7d4925ecc158bdfb2336c5624c8a0251e511
Suhaf Azam Butt <45166675+suhafbutt@users.noreply.github.com>
2025-10-07 22:31:49 +0200

    refactoring

--- src/components/DiaryEntry.jsx
+++ src/components/DiaryEntry.jsx
@@ -3,20 +3,21 @@ import locationImage from '../assets/images/location.png';
 
 
 export function DiaryEntry(props) {
+  const entry = props.entry;
   return (
     <section>
       <div className="location-detail-wraper">
-        <img src={props.location_image.src} className="city-image" alt={props.location_image.alt}/>
+        <img src={entry.location_image.src} className="city-image" alt={entry.location_image.alt}/>
         <div className="location-wrapper">
           <img src={locationImage} className="location-image" alt="globe icon"/>
-          <span className="city-name">{props.country_name}</span>
-          <a href={props.location_address} className="google-location" target="_blank">View on Google Maps</a>
+          <span className="city-name">{entry.country_name}</span>
+          <a href={entry.location_address} className="google-location" target="_blank">View on Google Maps</a>
           
         </div>
           
-        <h2 className="location-name">{props.location_name}</h2>
-        <div className="visit-date">{props.visit_date}</div>
-        <span className="location-description">{props.description}</span>
+        <h2 className="location-name">{entry.location_name}</h2>
+        <div className="visit-date">{entry.visit_date}</div>
+        <span className="location-description">{entry.description}</span>
       </div>
       <hr className="line-breaker"/>
     </section>
---
+++ src/data/diaryData.js
@@ -0,0 +1,42 @@
+import japanThumbnail from '../assets/images/japan.png';
+import sydneyOperaHouse from '../assets/images/sydney_opera_house.png';
+import geirangerfjord from '../assets/images/geirangerfjord.png';
+
+export default [
+  {
+    id: 'Pyu1u3k2cn',
+    location_image: {
+      src: japanThumbnail,
+      alt: "Mount Fuji"
+    },
+    country_name: "JAPAN",
+    location_address: "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
+    location_name: "Mount Fuji",
+    visit_date: "12 Jan, 2023 - 24 Jan, 2023",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+  },
+  {
+    id: 'P0THCeUBYS',
+    location_image: {
+      src: sydneyOperaHouse,
+      alt: "Sydney Opera House"
+    },
+    country_name: "AUSTRALIA",
+    location_address: "https://maps.app.goo.gl/QsKgJ97KdPdivhmw9",
+    location_name: "Sydney Opera House",
+    visit_date: "27 May, 2023 - 8 Jun, 2023",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+  },
+  {
+    id: 'HC8k0Fi03G',
+    location_image: {
+      src: geirangerfjord,
+      alt: "Geirangerfjord"
+    },
+    country_name: "NORWAY",
+    location_address: "https://maps.app.goo.gl/UFE3enAfkFrgYPbb6",
+    location_name: "Geirangerfjord",
+    visit_date: "01 Oct, 2024 - 18 Nov, 2024",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+  }
+]
